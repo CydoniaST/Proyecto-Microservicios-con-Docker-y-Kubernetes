297d2957bab9d3109eef03c401043aa687b6efbc
CydoniaST <mellado.acev.eric@gmail.com>
2024-02-01 12:03:47 +0100

    primer commit

---
+++ backend/eolopark/src/main/java/es/codeurjc/eolopark/EoloparkApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.eolopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoloparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoloparkApplication.class, args);
+	}
+
+}
---
+++ backend/eolopark/src/test/java/es/codeurjc/eolopark/EoloparkApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.eolopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoloparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
