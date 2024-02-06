3bd409f0ab5bc6f5901d5b5cf18b3ac3c8771b51
Sandraahm <127121398+Sandraahm@users.noreply.github.com>
2024-02-06 14:19:11 +0100

    changesGetterAndSetters

--- backend/eolopark/src/main/java/es/codeurjc/eolopark/model/Aerogenerator.java
+++ backend/eolopark/src/main/java/es/codeurjc/eolopark/model/Aerogenerator.java
@@ -4,15 +4,74 @@ public class Aerogenerator {
 
     private String Id;
 
-    private int Latitude;
+    private Int Latitude;
 
-    private int Longitude;
+    private Int Longitude;
 
-    private double BladeLength;
+    private Double BladeLength;
 
-    private double Height;
+    private Double Height;
 
-    private  double Power;
+    private  Double Power;
+
+
+  public Aerogenerator(String Id, Int Latitude, Int Longitude, Double BladeLength, Double Height, Double Power){
+    this.Id = Id;
+    this.Latitude= Latitude;
+    this.Longitude= Longitude;
+    this.BladeLength= BladeLength;
+    this.Height=Height;
+    this.Power= Power;
+
+  }
+
+   public String getId() {
+        return Id;
+    }
+
+    public  Int getLatitude(){
+        return Latitude;
+    }
+
+    public  Int Longitude(){
+        return Longitude;
+    }
+
+    public Double getBladeLength(){
+        return BladeLength;
+    }
+
+    public Double getHeigh(){
+        return Height;
+    }
+    
+    public Double getPower(){
+        return Power;
+    }
+     public void setId(String Id) {
+        this.Id = Id;
+    }
+
+      public void setId(Int Latitude) {
+        this.Latituded = Latitude;
+    }
+      public void setId(Int Longitude) {
+        this.Longitude = Longitude;
+    }
+      public void setId(Int Longitude) {
+        this.Longitude = Longitude;
+    }
+  public void setId(Double BladeLength) {
+        this.BladeLength = BladeLength;
+    }  
+     public void setId(Double Height) {
+        this.Height = Height;
+    }    
+     public void setId(Double Power) {
+        this.Power = Power;
+    }    
+
+
 
 
 }
