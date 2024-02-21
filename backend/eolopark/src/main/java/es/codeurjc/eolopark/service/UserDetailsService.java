0b10af745ced3d3e19d37b71ccd28e08a6e2555c
laurasanchez99 <118891317+laurasanchez99@users.noreply.github.com>
2024-02-21 14:57:19 +0100

    UserDetailsService and EoloPark.mwb

---
+++ backend/eolopark/src/main/java/es/codeurjc/eolopark/service/UserDetailsService.java
@@ -0,0 +1,75 @@
+package es.codeurjc.eolopark.Servicios;
+
+/*
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import es.codeurjc.eolopark.model.User;
+import es.codeurjc.eolopark.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerNewUser(String username, String password, String roles) {
+        if (userRepository.findByName(username).isPresent()) {
+            throw new RuntimeException("User already exists: " + username);
+        }
+
+        String encodedPassword = passwordEncoder.encode(password);
+        User newUser = new User(username, encodedPassword, roles);
+        return userRepository.save(newUser);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByName(username);
+    }
+}
+
+package es.codeurjc.eolopark.Servicios;
+*/
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import es.codeurjc.eolopark.model.User;
+import es.codeurjc.eolopark.repository.UserRepository;
+
+@Service
+public class UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerNewUser(String username, String password, String roles) {
+        if (userRepository.findByName(username).isPresent()) {
+            throw new RuntimeException("User already exists: " + username);
+        }
+
+        String encodedPassword = passwordEncoder.encode(password);
+        User newUser = new User(username, encodedPassword, roles);
+        return userRepository.save(newUser);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByName(username);
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID: " + id));
+    }
+
+}
