150b17d8dd208c3172794148bae55a77f5c80c87
CydoniaST <mellado.acev.eric@gmail.com>
2024-04-11 13:34:57 +0200

    Last version with api-docs

--- backend/eolopark/src/main/java/es/codeurjc/eolopark/configuration/SecurityConfiguration.java
+++ backend/eolopark/src/main/java/es/codeurjc/eolopark/configuration/SecurityConfiguration.java
@@ -111,6 +111,8 @@ public class SecurityConfiguration {
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers("/error").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/**").permitAll()
 
                         // PRIVATE PAGES
                         .requestMatchers("/DetailsPark/**").hasAnyRole("USER","ADMIN")
@@ -149,100 +151,4 @@ public class SecurityConfiguration {
 
         return http.build();
     }
-
-
-/*
-    @Bean
-    @Order(2)
-    public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
-        http.authenticationProvider(authenticationProvider());
-
-        http
-                .authorizeHttpRequests(authorize -> authorize
-
-                        // PUBLIC PAGES
-                        .requestMatchers("/index").permitAll()
-                        .requestMatchers("/register").permitAll()
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/favicon.ico").permitAll()
-                        .requestMatchers("/error").permitAll()
-
-                        // PRIVATE PAGES
-                        .requestMatchers("/DetailsPark/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/PaginaPrincipal").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/EoloPark").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/EoloPark/Automatic").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/EoloPark/Manual").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/EoloPark/delete/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/Successfully").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/EditEoloPark/Edit/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/NewAerogenerator/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/EditAerogenerator/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("EditAerogenerator/delete/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/NewSubstation/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/EditSubstation/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("Substation/").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("Substation/delete/**").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/private").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/admin/**").hasAnyRole("ADMIN")
-                        .requestMatchers("admin/makeUserPremium/{{user.id}}").hasAnyRole("ADMIN")
-                )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .failureUrl("/loginerror")
-                        .defaultSuccessUrl("/PaginaPrincipal")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                        .permitAll()
-                )
-                .httpBasic(withDefaults());
-
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
-
-    }
-*/
-    /*
-    @Bean
-    @Order(2)
-    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        http.authenticationProvider(authenticationProvider());
-
-        http
-                .securityMatcher("/api/**")
-                .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
-
-
-        http
-                .authorizeRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/api/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/api/**").permitAll()
-                        //
-                        //.requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole("USER", "ADMIN")
-                        //.requestMatchers(HttpMethod.PUT, "/api/**").hasAnyRole("USER", "ADMIN")
-                        //.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN")
-                        //.requestMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole("USER", "ADMIN")
-
-                )
-
-                .httpBasic(withDefaults());
-        //Disable Form login Authentication
-        http.formLogin(formLogin -> formLogin.disable());
-        // Disable CSRF protection (it is difficult to implement in REST APIs)
-        http.csrf(csrf -> csrf.disable());
-        // Enable Basic Authentication
-        http.httpBasic(Customizer.withDefaults());
-        // Stateless session
-        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
-
-    }
-    */
 }
